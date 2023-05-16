0666e7ed201d368a073c23f6e284e88154896a51
朱浩 <hao.zhu@shanbay.com>
2023-05-16 15:25:04 +0800

    feat: add api auth

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -2,11 +2,22 @@ import { prettyObject } from "@/app/utils/format";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "../../auth";
 import { requestOpenai } from "../../common";
+import { getToken } from "next-auth/jwt";
 
 async function handle(
   req: NextRequest,
   { params }: { params: { path: string[] } },
 ) {
+  const token = await getToken({ req });
+  console.log("[OpenAI Route] token ", token);
+  if (!token) {
+    return NextResponse.json(
+      { error: "Unauthorized" },
+      {
+        status: 401,
+      },
+    );
+  }
   console.log("[OpenAI Route] params ", params);
 
   const authResult = auth(req);
