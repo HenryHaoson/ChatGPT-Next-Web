3779a76e123545b25e1d9b20b8844afa593e4d03
朱浩 <hao.zhu@shanbay.com>
2023-06-06 17:41:11 +0800

    feat: common fetch

---
+++ app/utils/fetch.ts
@@ -0,0 +1,16 @@
+// next app router fetch, can be used in both server and client side，can config base url
+export const hFetch = async (url: string, options: any) => {
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : process.env.NEXT_PUBLIC_BASE_URL;
+  // check if is server side, if is server side, add cookie to request headers
+  if (typeof window === "undefined") {
+    const { cookies } = await import("next/headers");
+    options.headers = {
+      ...options.headers,
+      cookie: cookies().toString(),
+    };
+  }
+  return fetch(`${baseUrl}${url}`, options);
+};
